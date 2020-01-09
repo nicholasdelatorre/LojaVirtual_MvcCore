1fe41c7400694149367defe42f34ed3ebbe88fa6
Nicholas Dela Torre <nicholas.delatorre@42labs.com.br>
2020-01-09 18:28:09 -0300

    Add Smtp

--- LojaVirtual/Controllers/HomeController.cs
+++ LojaVirtual/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LojaVirtual.Libraries.Email;
+using LojaVirtual.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LojaVirtual.Controllers
@@ -16,6 +18,18 @@ namespace LojaVirtual.Controllers
         public IActionResult Contato()
         {
             return View();
+        }
+        public IActionResult ContatoAcao()
+        {
+            Contato contato = new Contato();
+
+            contato.Nome = HttpContext.Request.Form["nome"];
+            contato.Email = HttpContext.Request.Form["email"];
+            contato.Texto = HttpContext.Request.Form["texto"];
+
+            ContatoEmail.EnviarContatoPorEmail(contato);
+
+            return new ContentResult() { Content = string.Format("Dados recebidos com sucesso <br/>  Nome: {0} <br/> E-mail: {1} <br/>  Texto: {2}", contato.Nome, contato.Email, contato.Texto) , ContentType = "text/html"};
         }
 
         public IActionResult Login()
---
+++ LojaVirtual/Libraries/Email/ContatoEmail.cs
@@ -0,0 +1,65 @@
+﻿using LojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Email
+{
+    public static class ContatoEmail
+    {
+        public static void EnviarContatoPorEmail(Contato contato)
+        {
+            try
+            {
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<h2>Contato - LojaVirtual</h2>");
+                sb.Append(string.Format("<b>Nome: </b> {0} <br/>", contato.Nome));
+                sb.Append(string.Format("<b>E-mail: </b> {0} <br/>", contato.Email));
+                sb.Append(string.Format("<b>Texto: </b> {0} <br/>", contato.Texto));
+                sb.Append(" <br/> E-mail envialdo automaticamente do site LojaVirtual");
+
+
+                /*
+                 * MailMessage -> Message Constructor
+                 */
+
+                MailMessage mensagem = new MailMessage();
+                mensagem.From = new MailAddress("nicholas.delatorre@gmail.com");
+                mensagem.To.Add("nicholas.delatorre@gmail.com");
+                mensagem.Subject = "Contato - LojaVirtual - E-mail: " + contato.Email;
+                mensagem.Body = sb.ToString();
+                mensagem.IsBodyHtml = true;
+
+
+                /*
+                 * SMTP -> Email Sender Server
+                 */
+
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.Credentials = new NetworkCredential("nicholas.delatorre@gmail.com", "ra006510");
+                    smtp.EnableSsl = true;
+
+
+                    smtp.Send(mensagem);
+                }
+
+            }
+            catch (SmtpException ex)
+            {
+
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
