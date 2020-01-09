4e4def195df52efdbb9e72dd2d0eec777e126b03
Nicholas Dela Torre <nicholas.delatorre@42labs.com.br>
2020-01-09 10:47:05 -0300

    commit inicial

---
+++ LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Visualizar()
+        {
+
+            Produto produto = GetProduto();
+
+            return View(produto);
+            //return new ContentResult() { Content = "<h3>Produto -> Visualizar</h3>", ContentType = "text/html"};
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto
+            {
+                Id = 1,
+                Descricao = "Jogue em 4K",
+                Nome = "Xbox One X",
+                Valor = 2000.00M
+            };
+        }
+    }
+}
